2aefc6eb84c4d94138de31e35de27e401d3b042d
Kefiloe Ketja <kefiloedev@gmail.com>
2026-03-16 19:23:46 +0200

    fix nav props

--- app/Navbar/Nav.tsx
+++ app/Navbar/Nav.tsx
@@ -1,26 +1,24 @@
-"use client"; // ensure this is client-side
+"use client";
 
 import Logo from "@/app/components/Helper/Logo";
 import { NAVLINKS } from "@/constant/conatant";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { HiBars3BottomRight } from "react-icons/hi2";
-import MobileNav from "./MobileNav"; // make sure this path is correct
 
-const Nav = () => {
-  // State for mobile nav
-  const [showNav, setShowNav] = useState(false);
+type Props = {
+  openNav: () => void;
+};
 
-  const openNav = () => setShowNav(true);
-  const closeNav = () => setShowNav(false);
-
+const Nav = ({ openNav }: Props) => {
   return (
     <nav className="h-[12vh] relative z-50 lg:mt-8">
       <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
+        
         {/* LOGO */}
         <Logo />
 
-        {/* Nav links for large screens */}
+        {/* Desktop Nav */}
         <div className="hidden lg:flex bg-white h-[10vh] md:pl-4 items-center space-x-10 p-2">
           {NAVLINKS.map((link) => (
             <Link
@@ -31,23 +29,14 @@ const Nav = () => {
               <p>{link.lable}</p>
             </Link>
           ))}
+        </div>
 
-           {/* Contact Button */}
-          {/* <Link href="/Contact">
-            <button className="h-full p-4.5 bg-[#b69974] cursor-pointer text-white font-bold">
-              Contact Us
-            </button>
-          </Link> */}
-        </div> 
-
-        {/* Hamburger Icon for mobile */}
+        {/* Mobile Hamburger */}
         <div onClick={openNav} className="lg:hidden">
           <HiBars3BottomRight className="w-9 h-9 text-[#dfc39f] cursor-pointer" />
         </div>
-      </div>
 
-      {/* Mobile Navigation */}
-      <MobileNav showNav={showNav} closeNav={closeNav} />
+      </div>
     </nav>
   );
 };
